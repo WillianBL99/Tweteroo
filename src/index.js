9b8edcb67bde152e62a7ba21e05ce0716cd733b1
paulo uilian <uilianlbarros@gmail.com>
2022-04-23 12:13:43 -0300

    Implementará verificação do avatar de cada usuário

--- src/index.js
+++ src/index.js
@@ -2,10 +2,41 @@ import express, {json} from 'express';
 import cors from 'cors';
 import chalk from 'chalk';
 
+const userLogin = [];
+const tweets = [];
+
 const app = express();
 app.use(cors());
 app.use(json());
 
 app.listen(5000, () => {
     console.log(chalk.green.bold('Is running...'));
+});
+
+app.post('/sign-up', (req, res) => {
+    const {body} = req;
+    const {username, avatar} = body;
+    const login = {username, avatar};
+    userLogin.push(login);
+    
+    res.send('ok');
+});
+
+app.post('/tweets', (req, res) => {
+    const {body} = req;
+    const {username, tweet} = body;
+    const userTweet = {username, tweet};
+    tweets.push(userTweet);
+
+    res.send('ok');
+});
+
+app.get('/tweets', (req, res) => {
+    const tweetsToSend = tweets.slice(tweets.length - 10);
+    
+    res.send(tweetsToSend.map(tweetToSend => {
+        const {username, tweet} = tweetToSend;
+        const {avatar} = userLogin.find(user => user.username === username);
+        return {username, avatar, tweet};
+    }));
 });
