bafa9d1fc23ad6c8f826f7bea9d6fae889ce4db4
paulo uilian <uilianlbarros@gmail.com>
2022-04-23 15:13:27 -0300

    Validará username e avatar

--- src/Routes/sign_up.js
+++ src/Routes/sign_up.js
@@ -1,12 +1,30 @@
 import users_login from '../Models/users_login.js';
 
+const validateName = (name) => {
+    const isEmpty = name === "";
+    const isNull = name === null;
+    const isString = typeof(name) === 'string';
+
+    return !isEmpty && !isNull && isString;
+}
+
+const validateAvatar = (avatar) => {
+    return avatar?true:false;
+}
+
 const sing_up = (req, res) => {
     const {body} = req;
     const {username, avatar} = body;
-    const login = {username, avatar};
-    users_login.push(login);
-        
-    res.send('ok');
+
+    if(validateAvatar(avatar) && validateName(username)){
+        const login = {username, avatar};
+        users_login.push(login);
+            
+        res.send('ok');
+
+    } else {
+        res.status(400).send('Todos os campos são obrigatórios');
+    }
 }
 
 export default sing_up;
