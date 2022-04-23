97b584b5753829b069c38e7321e18860f73f7474
paulo uilian <uilianlbarros@gmail.com>
2022-04-23 10:46:37 -0300

    Primeiro commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import express, {json} from 'express';
+import cors from 'cors';
+import chalk from 'chalk';
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+app.listen(5000, () => {
+    console.log(chalk.green.bold('Is running...'));
+});
